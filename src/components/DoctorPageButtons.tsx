eb332de9277d6c7cbf4605eca2bfa7248131435c
Luis <simosa37@gmail.com>
2023-12-20 17:01:52 -0300

    profile picture design change

--- src/components/DoctorPageButtons.tsx
+++ src/components/DoctorPageButtons.tsx
@@ -1,5 +1,8 @@
 import { Tabs } from "@/types";
 import React from "react";
+import VideoCamSvg from "./icons/VideoCamSvg";
+import EnvelopeSvg from "./icons/EnvelopeSvg";
+import ClockSvg from "./icons/ClockSvg";
 
 const DoctorPageButtons = ({
   setCurrentTab,
@@ -38,18 +41,21 @@ const DoctorPageButtons = ({
       </ul>
       <ul className="flex gap-4 justify-center md:justify-end items-center">
         <li>
-          <button className="bg-blue-300 shadow-sm px-2 py-1 rounded-md">
-            Comenta
+          <button className="bg-blue-300 shadow-sm px-2 py-1 rounded-md flex items-center gap-1">
+            <EnvelopeSvg fill="black" />
+            <span>Mensaje</span>
           </button>
         </li>
         <li>
-          <button className="bg-blue-300 shadow-sm px-2 py-1 rounded-md">
-            Ver teléfono
+          <button className="bg-blue-300 shadow-sm px-2 py-1 rounded-md flex items-center gap-1">
+            <VideoCamSvg />
+            <span>Videollamada</span>
           </button>
         </li>
         <li>
-          <button className="bg-blue-300 shadow-sm px-2 py-1 rounded-md">
-            Solicitar turno
+          <button className="bg-blue-300 shadow-sm px-2 py-1 rounded-md flex items-center gap-1">
+            <ClockSvg />
+            <span>Solicitar turno</span>
           </button>
         </li>
       </ul>
--- src/components/DoctorPageHeader.tsx
+++ src/components/DoctorPageHeader.tsx
@@ -1,20 +1,34 @@
+import Image from "next/image";
 import React from "react";
 
 const DoctorPageHeader = () => {
   return (
     <div className="p-4 flex flex-col items-end bg-white md:flex-row">
-      <div className="w-80 md:w-96 h-80 md:h-96 bg-gray-300 mx-auto md:mx-0 "></div>
-      <div className="mx-auto md:ml-12">
-        <h1 className="text-4xl font-semibold mb-2">Nombre Apellido</h1>
-        <ul className="flex gap-2 text-sm justify-center md:justify-start">
-          <li className=" px-2 py-1 font-semibold bg-blue-200 rounded-md">
-            especialidad 1
-          </li>
-          <li className=" px-2 py-1 font-semibold bg-blue-200 rounded-md">
-            especialidad 2
-          </li>
-        </ul>
+      <div className="relative mx-auto md:mx-0">
+        <div className="w-80 md:w-96 h-[400px] md:h-96 bg-gray-300 mb-2 overflow-hidden z-0 ">
+          <Image
+            src="/profile.jpg"
+            alt="profile picture"
+            width={320}
+            height={320}
+          />
+        </div>
+        <div className="absolute top-0 w-full h-full bg-gradient-to-t from-black via-transparent z-10"></div>
+        <div className="absolute bottom-4 left-4 z-20">
+          <h1 className="text-5xl font-semibold text-white mb-2">
+            Nombre <br /> Apellido
+          </h1>
+          <ul className="flex gap-2 text-sm justify-center md:justify-start ">
+            <li className=" px-2 py-1 font-semibold bg-blue-200 rounded-md">
+              especialidad 1
+            </li>
+            <li className=" px-2 py-1 font-semibold bg-blue-200 rounded-md">
+              especialidad 2
+            </li>
+          </ul>
+        </div>
       </div>
+      <div className="mx-auto md:ml-12"></div>
     </div>
   );
 };
